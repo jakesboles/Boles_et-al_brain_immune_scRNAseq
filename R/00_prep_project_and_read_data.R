dfda17d0d6b9179273b9b23473cc5d90a2b9a9f1
jakesboles <jake.boles@ufl.edu>
2023-10-11 16:16:11 -0400

    update prep project script - still need to include instructions to download GEO data after GEO submission is online

--- R/00_prep_project_and_read_data.R
+++ R/00_prep_project_and_read_data.R
@@ -1,9 +1,10 @@
 #Prepare directories as needed and load in data from our GEO submission ----
 
-dir.create("01_raw_data")
+dir.create(c("raw_data", "data_objects", "plots", "tabular_output", "chooseR",
+             "cross_entropy_test", "cluster_annotation"))
 
 for (i in seq_along(1:8)){
-  mkdir(paste0("01_raw_data/sample", i))
+  mkdir(paste0("raw_data/sample", i))
   
   #add data download instructions here
 }
