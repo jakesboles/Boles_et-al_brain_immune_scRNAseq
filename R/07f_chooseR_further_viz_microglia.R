e1fb7a28fb8b1d34e4d1444c728e064d8a1bdea6
jakesboles <jake.boles@ufl.edu>
2023-10-18 09:16:02 -0400

    added plotting, Dice coefficient computation, and named plot files appropriately

--- R/07f_chooseR_further_viz_microglia.R
+++ R/07f_chooseR_further_viz_microglia.R
@@ -59,7 +59,7 @@ plot
 
 ggsave(
   plot = plot,
-  filename = paste0(results_path, "coclustering_heatmap_", choice, ".png"),
+  filename = paste0(plots, "Fig_S6C.png"),
   dpi = 300,
   height = 3.5,
   width = 3.5,
@@ -97,9 +97,100 @@ plot
 
 ggsave(
   plot = plot,
-  filename = paste0(results_path, choice, "_silhouette_umap.png"),
+  filename = paste0(plots, "Fig_S6B.png"),
   dpi = 300,
   height = 5,
   width = 5.5,
   units = "in"
 )
+
+#Below we'll generate a few plots for Supplemental Figure 6
+theme <- theme(axis.text = element_blank(),   #thematic elements for plots
+               axis.ticks = element_blank(),
+               axis.line = element_line(arrow = arrow(angle = 15, 
+                                                      length = unit(0.5, "cm"), 
+                                                      type = "closed")))
+
+plots <- "chooseR/microglia/"
+
+#Labeled DimPlot in downsampled data
+downsamp <- obj
+Idents(downsamp) <- downsamp@meta.data$pca.SCT_res.0.4
+
+DimPlot_scCustom(downsamp,
+                 label = T,
+                 colors_use = ColorBlind_Pal(),
+                 label.size = 8) +
+  theme
+ggsave(paste0(plots, "Fig_S6A.png"),
+       units = "in", dpi = 600,
+       height = 5, width = 5.5)
+
+#Labeled DimPlot in full data
+obj <- readRDS("data_objects/05_clean_microglia_sub.RDS")
+
+DefaultAssay(obj) <- "integrated"
+
+obj <- FindClusters(obj, resolution = 0.4)
+
+DimPlot_scCustom(obj, label = T,
+                 pt.size = 1,
+                 label.size = 8,
+                 repel = T) +
+  theme
+ggsave(paste0(plots, "Fig_S6E.png"),
+       units = "in", dpi = 600,
+       height = 5, width = 5.5)
+
+#Compute dice coefficient between cluster assignment in full and downsampled data
+obj@meta.data -> full_meta
+downsamp@meta.data -> sampled_meta
+
+full_meta %>%
+  dplyr::select(c(integrated_snn_res.0.4)) %>%
+  rownames_to_column(var = "cell") -> full_meta
+sampled_meta %>%
+  dplyr::select(pca.SCT_res.0.4) %>%
+  rownames_to_column(var = "cell") -> sampled_meta
+
+sampled_meta %>%
+  left_join(full_meta, by = "cell") %>%
+  group_by(pca.SCT_res.0.4, integrated_snn_res.0.4) %>%
+  summarize(count = n()) %>%
+  ungroup() %>%
+  complete(pca.SCT_res.0.4, integrated_snn_res.0.4, fill = list("count" = 0)) %>%
+  group_by(pca.SCT_res.0.4) %>%
+  mutate(n_sampled = sum(count)) %>%
+  ungroup() %>%
+  group_by(integrated_snn_res.0.4) %>%
+  mutate(n_full = sum(count)) %>%
+  ungroup() %>%
+  mutate(dice = (2 * count) / (n_full + n_sampled)) %>%
+  mutate(
+    pca.SCT_res.0.4 = factor(pca.SCT_res.0.4, 
+                             levels = c(0:6)),
+    integrated_snn_res.0.4 = factor(integrated_snn_res.0.4, 
+                                    levels = c(0:13))) %>%
+  ggplot(aes(y = pca.SCT_res.0.4, x = integrated_snn_res.0.4)) + 
+  geom_tile(color = "white",
+            aes(fill = dice)) + 
+  scale_y_discrete("Down-sampled chooseR clusters", expand = c(0, 0)) +
+  scale_x_discrete(
+    "Equivalent resolution in full dataset",
+    expand = c(0, 0)) +
+  scale_fill_paletteer_c("grDevices::Inferno",
+                         direction = 1) +
+  labs(fill = "Dice\ncoefficient") +
+  theme(
+    axis.ticks = element_blank(),
+    axis.text = element_text(size = 8, color = "black"),
+    axis.title = element_text(size = 9),
+    legend.text = element_text(size = 8),
+    legend.title = element_text(size = 8, hjust = 0),
+    plot.title = element_text(hjust = 0.5, size = 12, face = "bold")
+  )
+ggsave(filename = paste0(plots, "Fig_S6D.png"),
+       dpi = 300,
+       height = 2.5,
+       width = 3.5,
+       units = "in")
