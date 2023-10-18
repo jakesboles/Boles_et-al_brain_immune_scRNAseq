f1f863dcbe64b1f0a738e531e541f5b53f00868c
jakesboles <jake.boles@ufl.edu>
2023-10-18 10:08:37 -0400

    object loading

--- R/08a_bcells_annotation_Fig_S3I_S4D-I.R
+++ R/08a_bcells_annotation_Fig_S3I_S4D-I.R
@@ -1,4 +1,8 @@
 library(Seurat)
 library(scCustomize)
+library(patchwork)
+library(dittoSeq)
 library(tidyverse)
+library(paletteer)
 
+obj <- readRDS("data_objects/05_clean_bcells_sub.RDS")
--- R/08b_mono-macs_annotation_Fig_S3L_S7D-P.R
+++ R/08b_mono-macs_annotation_Fig_S3L_S7D-P.R
@@ -1,4 +1,11 @@
 library(Seurat)
 library(scCustomize)
+library(patchwork)
+library(dittoSeq)
 library(tidyverse)
+library(paletteer)
 
+obj <- readRDS("data_objects/05_clean_mono-macs_sub.RDS")
+
+#Need to run PrepSCTFindMarkers because the data were integrated in 05
+obj <- PrepSCTFindMarkers(obj)
