38eb7d8ba67ab992f8de7994ce811e92b1a5a603
jakesboles <jake.boles@ufl.edu>
2023-10-18 10:22:21 -0400

    added unicolor DimPlot for Fig S3

--- R/08d_tnk_annotation_Fig_S3N_S9D-P.R
+++ R/08d_tnk_annotation_Fig_S3N_S9D-P.R
@@ -6,3 +6,32 @@ library(tidyverse)
 library(paletteer)
 
 obj <- readRDS("data_objects/05_clean_tnk_sub.RDS")
+
+dir.create("plots/03_fine_cluster_id")
+plots <- "plots/03_fine_cluster_id/"
+
+#Make unicolored plot for Fig S3 ----
+pal <- dittoColors()
+col <- pal[24]
+
+theme <- theme(axis.text = element_blank(),
+               axis.ticks = element_blank(),
+               axis.line = element_line(arrow = arrow(angle = 15, 
+                                                      length = unit(0.5, "cm"), 
+                                                      type = "closed")))
+
+obj@meta.data <- obj@meta.data %>%
+  mutate(sfig3 = 1)
+DimPlot_scCustom(obj,
+                 label = F,
+                 group.by = "sfig3",
+                 colors_use = col,
+                 pt.size = 2) + 
+  theme(
+    legend.position = "none",
+    plot.title = element_blank()
+  ) +
+  theme
+ggsave(paste0(plots, "Fig_S3N.png"),
+       units = "in", dpi = 600,
+       height = 3, width = 3)
