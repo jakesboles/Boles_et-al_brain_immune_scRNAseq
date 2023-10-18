8594039a6eee2d24ee92e357b7094e807033d4be
jakesboles <jake.boles@ufl.edu>
2023-10-18 10:32:13 -0400

    added marker curation and feature plots - need to include actual annotations for S4I

--- R/08a_bcells_annotation_Fig_S3I_S4D-I.R
+++ R/08a_bcells_annotation_Fig_S3I_S4D-I.R
@@ -35,3 +35,41 @@ DimPlot_scCustom(obj,
 ggsave(paste0(plots, "Fig_S3I.png"),
        units = "in", dpi = 600,
        height = 3, width = 3)
+
+#Generate markers file ----
+obj <- FindClusters(obj, 
+                    resolution = 0.6) #resolution comes from chooseR
+
+markers <- FindAllMarkers(obj, only.pos = F)
+
+write.csv(markers,
+          file = "tabular_data/bcells_markers.csv")
+
+#Canonical cell markers FeaturePlots based on markers file  ----
+theme <- theme(axis.text = element_blank(),
+               axis.ticks = element_blank(),
+               axis.line = element_line(arrow = arrow(angle = 15, 
+                                                      length = unit(0.5, "cm"), 
+                                                      type = "closed")))
+
+fplot <- function(gene, filename){
+  p <- FeaturePlot_scCustom(obj,
+                            features = gene,
+                            colors_use = viridis_light_high,
+                            na_cutoff = 1) + 
+    theme(
+      plot.title = element_text(face = "bold.italic")
+    ) +
+    theme
+  
+  ggsave(p,
+         filename = paste0(plots, filename, ".png"),
+         units = "in", dpi = 600,
+         height = 4, width = 4.5)
+}
+
+fplot("Cd79a", "Fig_S4D")
+fplot("Ms4a1", "Fig_S4E")
+fplot("Ighm", "Fig_S4F")
+fplot("Cd3e", "Fig_S4G")
+fplot("Nkg7", "Fig_S4H")
