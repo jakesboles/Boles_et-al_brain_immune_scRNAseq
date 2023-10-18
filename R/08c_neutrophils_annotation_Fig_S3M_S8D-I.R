768223f49e7a41b2de8926f36c234bde5b782712
jakesboles <jake.boles@ufl.edu>
2023-10-18 10:51:37 -0400

    marker curation and cell marker plots - still need annotations

--- R/08c_neutrophils_annotation_Fig_S3M_S8D-I.R
+++ R/08c_neutrophils_annotation_Fig_S3M_S8D-I.R
@@ -37,3 +37,40 @@ DimPlot_scCustom(obj,
 ggsave(paste0(plots, "Fig_S3M.png"),
        units = "in", dpi = 600,
        height = 3, width = 3)
+
+#Generate markers file ----
+obj <- FindClusters(obj,
+                    resolution = 0.6)
+
+markers <- FindAllMarkers(obj, only.pos = F)
+
+write.csv(markers,
+          file = "tabular_data/neutrophils_markers.csv")
+
+#Canonical cell markers FeaturePlots based on markers file  ----
+theme <- theme(axis.text = element_blank(),
+               axis.ticks = element_blank(),
+               axis.line = element_line(arrow = arrow(angle = 15, 
+                                                      length = unit(0.5, "cm"), 
+                                                      type = "closed")))
+fplot <- function(gene, filename){
+  p <- FeaturePlot_scCustom(obj,
+                            features = gene,
+                            colors_use = viridis_light_high,
+                            na_cutoff = 1) + 
+    theme(
+      plot.title = element_text(face = "bold.italic")
+    ) +
+    theme
+  
+  ggsave(p,
+         filename = paste0(plots, filename, ".png"),
+         units = "in", dpi = 600,
+         height = 4, width = 4.5)
+}
+
+fplot("Mmp8", "Fig_S8D")
+fplot("Ngp", "Fig_S8E")
+fplot("S100a8", "Fig_S8F")
+fplot("S100a9", "Fig_S8G")
+fplot("P2ry12", "Fig_S8H")
